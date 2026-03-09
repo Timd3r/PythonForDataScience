6e8c7a85a003b4f5108f918386afb7cd6ccf8ab7
Tim De Raeve <tde-raev@a2-r2-p7.s19.be>
2026-03-09 12:43:48 +0100

    added doc to ex00

--- Python_01_Array/ex00/give_bmi.py
+++ Python_01_Array/ex00/give_bmi.py
@@ -1,8 +1,13 @@
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def give_bmi(height: list[int | float],
+             weight: list[int | float]) -> list[int | float]:
+    """Calculate the BMI for each pair of height and weight."""
     bmi = []
     for h, w in zip(height, weight):
         bmi.append(w / (h ** 2))
     return bmi
 
+
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """Return a list of booleans
+    indicating whether each BMI exceeds the limit."""
     return [b > limit for b in bmi]
