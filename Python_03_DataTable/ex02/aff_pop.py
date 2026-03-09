2750049f7b7f97ef74d063b1a17333a290053c91
Tim De Raeve <tde-raev@a2-r2-p7.s19.be>
2026-03-09 18:41:37 +0100

    fixed ex02

--- Python_03_DataTable/ex02/aff_pop.py
+++ Python_03_DataTable/ex02/aff_pop.py
@@ -1,34 +1,41 @@
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
 from load_csv import load
 
 
-def millions_formatter(x, pos):
-    return f"{int(x)}M"
+def to_millions(val):
+    if isinstance(val, str):
+        if 'M' in val:
+            return float(val.replace('M', ''))
+        elif 'k' in val:
+            return float(val.replace('k', '')) / 1000
+    return float(val)
 
 
 def aff_pop():
     try:
-        df = load("Python_03_DataTable/data/life_expectancy_years.csv")
+        df = load("Python_03_DataTable/data/population_total.csv")
         if df.empty:
             print("No data to plot.")
             return
 
         Belgium_data = df[df["country"] == "Belgium"]
         Zimbabwe_data = df[df["country"] == "Zimbabwe"]
-        le_belgium = Belgium_data.iloc[0, 1:].to_numpy()
-        le_zimbabwe = Zimbabwe_data.iloc[0, 1:].to_numpy()
+        le_belgium = Belgium_data.iloc[0, 1:]
+        le_zimbabwe = Zimbabwe_data.iloc[0, 1:]
+        le_belgium = le_belgium.apply(to_millions).to_numpy()
+        le_zimbabwe = le_zimbabwe.apply(to_millions).to_numpy()
         years = Belgium_data.columns[1:].to_numpy()
 
         plt.figure(figsize=(10, 6))
         plt.plot(years[0:250], le_belgium[0:250], label="Belgium")
         plt.plot(years[0:250], le_zimbabwe[0:250], label="Zimbabwe")
-        plt.gca().yaxis.set_major_formatter(FuncFormatter(millions_formatter))
         plt.title("Population projections")
         plt.xlabel("Year")
         plt.ylabel("Population", labelpad=20)
         plt.xticks(years[0:250:40])
-        plt.yticks(range(0, 80, 20))
+        yticks_values = [0, 5, 10, 15, 20, 25]
+        yticks_labels = ["0M", "5M", "10M", "15M", "20M", "25M"]
+        plt.yticks(yticks_values, yticks_labels)
         plt.grid(False)
         plt.legend(loc="lower right")
         plt.show()
